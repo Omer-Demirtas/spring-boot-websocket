931bac763a2ddad7505bef4eae0c73f87b538f67
Omer Demirtas <omer.demirtas@toyotatr.com>
2023-05-07 23:10:53 +0300

    Adding token validator

--- backend/src/main/java/com/example/websocket/config/WebSocketConfig.java
+++ backend/src/main/java/com/example/websocket/config/WebSocketConfig.java
@@ -4,6 +4,8 @@ import com.example.websocket.repository.NotificationUserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.config.ChannelRegistration;
@@ -11,10 +13,15 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Map;
 import java.util.Objects;
 
 @Log4j2
@@ -27,7 +34,39 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry
+                .addEndpoint("/ws")
+                .addInterceptors(new HandshakeInterceptor() {
+                    @Override
+                    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+                        String authorizationHeader = request.getHeaders().getFirst("Authorization");
+
+                        MultiValueMap<String, String> queryParams = UriComponentsBuilder.fromUri(request.getURI()).build().getQueryParams();
+                        String token = queryParams.getFirst("token");
+
+                        if (token != null) {
+                            log.info("TOKEN {}",  token);
+                            return true;
+                        } else {
+                            return false;
+                        }
+                        /*
+                        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                            String jwtToken = authorizationHeader.substring(7);
+                            //Authentication authentication = JwtUtils.parseJwtToken(jwtToken); // Assuming you have a JwtUtils class that can parse the JWT token and return an Authentication object.
+                            //SecurityContextHolder.getContext().setAuthentication(authentication);
+                        } else {
+                            return false;
+                        }
+
+                        */
+                    }
+
+                    @Override
+                    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+                    }
+                })
+                .setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
