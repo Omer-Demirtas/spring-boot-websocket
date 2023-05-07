4bbf474d4b278291e28bd190606700c7ac38fb0a
Omer Demirtas <omer.demirtas@toyotatr.com>
2023-05-07 23:32:32 +0300

    UI component updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
+import Button from "./components/Button";
+import ButtonGroup from "./components/ButtonGroup";
+import Container from "./components/Container";
+import TextInput from "./components/TextInput";
 
 function App() {
   return (
-    <div className="App">
-        
-    </div>
+    <Container>
+      <TextInput placeholder={"type your id"} />
+      <ButtonGroup>
+        <Button>Connect</Button>
+      </ButtonGroup>
+    </Container>
   );
 }
 
