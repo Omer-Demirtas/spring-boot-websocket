0fc8a46eb2558f1c86877b88373d350190a20747
Omer Demirtas <omer.demirtas@toyotatr.com>
2023-05-08 13:12:29 +0300

    websocket connection code added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,41 @@
+import SockJS from "sockjs-client";
 import Button from "./components/Button";
 import ButtonGroup from "./components/ButtonGroup";
 import Container from "./components/Container";
 import TextInput from "./components/TextInput";
+import { useState } from "react";
+import { over } from "stompjs";
+
+var stompClient =null;
 
 function App() {
   const token = "asasdasd";
+  const [status, setStatus] = useState(false);
+  const [username, setUsername] = useState('');
+  
+  const connect = () => {
+    let Sock = new SockJS(`http://localhost:8080/ws?token=${token}`);
+    stompClient = over(Sock);
+    stompClient.connect({ login: "1", token }, onConnected, onError);
+  };
+
+  const onConnected = () => {
+    setStatus(true)
+    stompClient.subscribe("/chatroom/public", onMessageReceived);
+    stompClient.subscribe(`/user/${username}/private`, onPrivateMessage);
+  };
+
+  const onError = (err) => {
+    console.log(err);
+  }
+
+  const onMessageReceived = (msg) => {
+    console.log({msg});
+  }
+
+  const onPrivateMessage = (msg) => {
+    console.log({msg});
+  }
 
   return (
     <Container>
@@ -16,6 +47,7 @@ function App() {
 
           console.log({ username });
 
+          setUsername(username);
           connect();
         }}
       >
